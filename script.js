7e701cb3bef506081888395d2e14e7f26a1f2cc7
Qwarkeh <christine.f.stark@gmail.com>
2022-02-04 13:12:39 -0500

    completed subtract, multiply, divide, and operate functions

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-// wait for button press to assign first number
-//let numOne = NUMBER_BUTTON_PRESS
-let numOne = 5;
-
-let a = 1;
-
-function add (numOne) {
-
-    //event handler that listens for + click
-
-    //const numTwo = event handler that listens for another button to be pressed
-    const numTwo = 3;
-    
-    //change display number to equal answer
+function add () {
     let answer = numOne + numTwo;
-
-
+    console.log(`Answer: ${answer}`);
     numOne = answer;
-
 }
 
-add(numOne);
+function subtract() {
+    let answer = numOne - numTwo;
+    console.log(`Answer: ${answer}`);
+    numOne = answer;
+}
+
+function multiply() {
+    let answer = numOne * numTwo;
+    console.log(`Answer: ${answer}`);
+    numOne = answer;
+}
+
+function divide () {
+    let answer = numOne / numTwo;
+    console.log(`Answer: ${answer}`);
+    numOne = answer;
+}
+
+function operate (numOne, operation, numTwo ){
+    if (operation === '+') add();
+    if (operation === '-') subtract();
+    if (operation === '*') multiply();
+    if (operation === '/') divide();
+}
+
+//THREE EVENT HANDLERS, ONE FOR EACH BUTTON
+//listen for first number press
+//listen for operation press
+//listen for second number
+//run the above values through operate();
+
+//test area
+let numOne = 2;
+let operation = '/';
+let numTwo = 3;
+ 
+operate(numOne,operation,numTwo);
+console.log(numOne);
