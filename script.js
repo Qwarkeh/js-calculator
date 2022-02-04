0f8ee3c615428c7b53ecc194a609860d6ce86fbf
Qwarkeh <christine.f.stark@gmail.com>
2022-02-04 11:51:36 -0500

    added 'add' function, not hooked up to ui

---
+++ script.js
@@ -0,0 +1,22 @@
+// wait for button press to assign first number
+//let numOne = NUMBER_BUTTON_PRESS
+let numOne = 5;
+
+let a = 1;
+
+function add (numOne) {
+
+    //event handler that listens for + click
+
+    //const numTwo = event handler that listens for another button to be pressed
+    const numTwo = 3;
+    
+    //change display number to equal answer
+    let answer = numOne + numTwo;
+
+
+    numOne = answer;
+
+}
+
+add(numOne);
