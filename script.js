23d659638e0d48a4170f257df2cca34ca0cfbe7b
Qwarkeh <christine.f.stark@gmail.com>
2022-02-04 16:26:09 -0500

    mapped event handlers, each button outputs it's id on click

--- script.js
+++ script.js
@@ -35,10 +35,47 @@ function operate (numOne, operation, numTwo ){
 //listen for second number
 //run the above values through operate();
 
+function getId (clicked_id) {
+   return clicked_id;
+}
+
+function handlers () {
+
+    // number buttons
+    const numberButtons = document.querySelectorAll('.number');
+
+    numberButtons.forEach(number => number.addEventListener('click',() => {
+        const numberId = number.id;
+        console.log(numberId);
+    } ))
+
+    // operation buttons
+    const operationButtons = document.querySelectorAll('.operation');
+    operationButtons.forEach(operation => operation.addEventListener('click', () => {
+       const operationId = operation.id;
+       console.log(operationId); 
+    }))
+
+    // AC
+    const allClearButton = document.querySelector('#AC');
+
+    allClearButton.addEventListener('click', () => {
+        const allClearId = allClearButton.id;
+        console.log(allClearId);
+    });
+
+    //decimal
+    const decimalButton = document.querySelector('#decimal');
+    
+    decimalButton.addEventListener('click', () => {
+        const decimalId = decimalButton.id;
+        console.log(decimalId);
+    })
+}
+
 //test area
 let numOne = 2;
 let operation = '/';
 let numTwo = 3;
  
-operate(numOne,operation,numTwo);
-console.log(numOne);
+handlers();
